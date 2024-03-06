a64ba5cc9f67dc57f088e73c91b0bb72475e9eb3
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-03-06 19:48:49 +0000

    code stuf

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Adam Batiuk All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adam Batiuk
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+  // Define the lengths of the three sides of a triangle
+  let sidea = document.getElementById('side-a');
+  let sideb = document.getElementById('side-b');
+  let sidec = document.getElementById('side-c');
+
+  // Calculate the semi-perimeter of the triangle
+  let s = (sidea + sideb + sidec) / 2;
+
+  // Use Heron's formula to calculate the area of the triangle
+  let area = Math.sqrt(s * ((s - sidea) * (s - sideb) * (s - sidec)));
+
+function myButtonClicked() {
+  alert(area)
+}
