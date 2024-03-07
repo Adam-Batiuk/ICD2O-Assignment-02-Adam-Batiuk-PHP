a01ee8b9e8a8209a471d7dfdc66b34924a845dab
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-03-07 19:23:31 +0000

    Done!

--- js/script.js
+++ js/script.js
@@ -3,18 +3,19 @@
 // Created by: Adam Batiuk
 // Created on: Mar 2024
 // This file contains the JS functions for index.html
-
-  // Define the lengths of the three sides of a triangle
-  let sidea = document.getElementById('side-a');
-  let sideb = document.getElementById('side-b');
-  let sidec = document.getElementById('side-c');
-
-  // Calculate the semi-perimeter of the triangle
-  let s = (sidea + sideb + sidec) / 2;
-
-  // Use Heron's formula to calculate the area of the triangle
-  let area = Math.sqrt(s * ((s - sidea) * (s - sideb) * (s - sidec)));
+"use strict"
 
 function myButtonClicked() {
-  alert(area)
+  // Define the lengths of the three sides of a triangle
+  const sidea = parseInt(document.getElementById("side-a").value)
+  const sideb = parseInt(document.getElementById("side-b").value)
+  const sidec = parseInt(document.getElementById("side-c").value)
+
+  //Calculate the semi-perimeter of the triangle
+  const s = (sidea + sideb + sidec) / 2
+
+  // Use Heron's formula to calculate the area of the triangle
+  const area = Math.sqrt(s * ((s - sidea) * (s - sideb) * (s - sidec)))
+
+  document.getElementById("answer").innerHTML = "Area is: " + area + " cm²"
 }
